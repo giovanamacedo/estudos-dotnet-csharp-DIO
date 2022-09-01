3df8c9079c000e862cfd72bd8c6cf87dbd4f6da8
giovanamacedo <giovanaml12@gmail.com>
2022-09-01 20:32:46 -0300

    organizando repositorio

---
+++ DIO/Construtores, Propriedades, Delegates e Eventos em .NET/ExemploConstrutores/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExemploConstrutores.Models;
+
+// Pessoa p1 = new Pessoa();
+// Pessoa p2 = new Pessoa("Giovana", "Macedo");
+// p2.Apresentar();
+
+Log log = Log.GetInstance();
+log.PropriedadeLog = "Teste instância";
+
+Log log2 = Log.GetInstance();
+System.Console.WriteLine(log2.PropriedadeLog);
